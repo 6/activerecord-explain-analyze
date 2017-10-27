faa290b3af4aaade4eac90ab8a0ba8720657363c
Peter Graham <peterghm@gmail.com>
2017-10-27 14:35:23 -0400

    make specs run

--- spec/activerecord/explain/analyze_spec.rb
+++ spec/activerecord/explain/analyze_spec.rb
@@ -1,11 +1,5 @@
-require "spec_helper"
-
-RSpec.describe Activerecord::Explain::Analyze do
+describe Activerecord::Explain::Analyze do
   it "has a version number" do
     expect(Activerecord::Explain::Analyze::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 require "bundler/setup"
 require "activerecord/explain/analyze"
+require 'rspec'
+require 'rspec/collection_matchers'
+require 'rspec/its'
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.expose_dsl_globally = true
+  config.run_all_when_everything_filtered = true
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
