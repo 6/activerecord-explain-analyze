14400b3d8ac03d4314ac0f912c78f793c366d409
Peter Graham <peterghm@gmail.com>
2023-01-12 17:05:13 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ def create_database
     dbname: 'postgres',
     host: db_config['host'],
     port: db_config['port'],
-    username: db_config['username'],
+    user: db_config['username'],
     password: db_config['password'],
   })
   pg_connection.exec(%{DROP DATABASE IF EXISTS "#{db_config['database']}";})
